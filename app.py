903e5ae9e6437d0be6185e3437082745313a4f00
shiv <shivk.v047@gmail.com>
2023-08-21 03:30:08 +0530

    added feature

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
-
+from flask import Flask,g
+from gurukool_bp import auth_bp
+from flask_session import Session
+from mongo.mongo import db
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = 'secret'
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['SESSION_FILE_THRESHOLD'] = 10
+Session(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! shiv rocks</p>"
-
+app.register_blueprint(auth_bp.auth_bp)
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0",debug=True)
- 
+    app.run(host="0.0.0.0", debug=True)
