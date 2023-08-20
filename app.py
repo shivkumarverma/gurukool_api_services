7131c74f95fe5d3c36509029e5c5e745aced3c31
shiv <shivk.v047@gmail.com>
2023-08-20 23:39:54 +0530

    m

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! shiv</p>"
+    return "<p>Hello, World! shiv rocks</p>"
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host="0.0.0.0",debug=True)
  
